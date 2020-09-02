e87039bfc6e784bb0120db3bf3373ebf09e40bbc
Sayako Hoshimiya <knight.of.lambda.calculus@gmail.com>
2020-09-02 19:13:03 +0800

    a

---
+++ Assets/Scripts/XMPPDie.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class XMPPDieRotation {
+    List<Quaternion> d20 = new List<Quaternion> {
+        Quaternion.identity,
+        // Quaternion.Euler(),
+    };
+}
+
+public class XMPPDie : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start() {
+        // var r1 = new Rotation
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
